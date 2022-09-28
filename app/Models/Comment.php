a0b47d0c6cca60c0f2b16829e2ae2b034c60a891
Lorin de Lepper <lorindelepper@gmail.com>
2022-09-28 11:10:55 +0200

    Section 10, opdracht 3

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,5 +9,13 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    public function post() // post_id
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author() // auther_id word overwritten met user_id
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
