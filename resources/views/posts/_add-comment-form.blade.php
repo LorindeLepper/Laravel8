39107cae3dd735712cd273edd12321d6518e2e1c
Lorin de Lepper <lorindelepper@gmail.com>
2022-09-30 10:47:54 +0200

    Section 12, opdracht 4

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -23,10 +23,7 @@
             </div>
 
             <div class="flex justify-end mt-6 pt-6 border-t border-gray-200 pt-6">
-                <button type="submit"
-                        class="bg-blue-500 text-white uppercase font-semibold text-sm py-2 px-10 rounded-2xl hover:bg-blue-600">
-                    Post
-                </button>
+                <x-form.button>Submit</x-form.button>
             </div>
         </form>
     </x-panel>
